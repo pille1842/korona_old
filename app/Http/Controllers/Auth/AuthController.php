f63eb7abc789928a2f1f0226b0ce59ef89e6ad21
Eric Haberstroh <eric@erixpage.de>
2016-06-15 23:35:58 +0200

    Füge Anmeldeformular per Artisan hinzu

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -63,10 +63,16 @@ class AuthController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => bcrypt($data['password']),
-        ]);
+        throw new Exception('Registrierung wurde abgeschaltet.');
+    }
+
+    public function showRegistrationForm()
+    {
+        return redirect('login');
+    }
+
+    public function register()
+    {
+        //
     }
 }
