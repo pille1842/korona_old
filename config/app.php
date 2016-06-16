3da58cafef0f5cecca84dc360dd4cc7a60c69fc3
Eric Haberstroh <eric@erixpage.de>
2016-06-16 12:23:02 +0200

    Füge einen Konfigurationswert für den Anwendungsnamen hinzu

--- config/app.php
+++ config/app.php
@@ -7,6 +7,18 @@ return [
     | Application Environment
     |--------------------------------------------------------------------------
     |
+    | This value determines the name of your application. It is displayed in
+    | various views, e.g. in the HTML title of your site.
+    |
+    */
+
+    'sitename' => env('APP_SITENAME', 'Korona'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Application Environment
+    |--------------------------------------------------------------------------
+    |
     | This value determines the "environment" your application is currently
     | running in. This may determine how you prefer to configure various
     | services your application utilizes. Set this in your ".env" file.
