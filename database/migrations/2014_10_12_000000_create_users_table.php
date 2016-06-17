aa9c5e75a2830dd7aa7cce7113eb399eccb1c3a2
Eric Haberstroh <eric@erixpage.de>
2016-06-17 19:50:39 +0200

    Ändere User-Model und Migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('handle')->unique();
+            $table->string('email');
             $table->string('password');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('nickname');
             $table->rememberToken();
             $table->timestamps();
         });
