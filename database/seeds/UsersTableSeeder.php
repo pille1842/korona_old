af01e182a07a51629ceae839afa45c7b98225d75
Eric Haberstroh <gpg@erixpage.de>
2016-09-28 20:44:32 +0200

    Don't seed posts anymore, use tidier fake users

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -18,51 +18,51 @@ class UsersTableSeeder extends Seeder
         Member::truncate();
 
         $user           = new User();
-        $user->username = 'Test1';
-        $user->password = Hash::make('test1');
-        $user->email    = 'test1@homestead.app';
-        $user->handle   = 'test1';
+        $user->username = 'Ajax';
+        $user->password = Hash::make('secret');
+        $user->email    = 'ajax@homestead.app';
+        $user->handle   = 'ajax';
         $user->active   = true;
         $user->save();
 
         $member             = new Member();
-        $member->firstname  = 'Testnutzer';
-        $member->lastname   = 'Eins';
-        $member->nickname   = 'Testus';
+        $member->firstname  = 'Max';
+        $member->lastname   = 'Mustermann';
+        $member->nickname   = 'Ajax';
         $member->profession = 'Softwaretester';
         $member->birthday   = Carbon::createFromDate(1991, 12, 18);
         $member->active     = true;
         $user->member()->save($member);
 
         $user2           = new User();
-        $user2->username = 'Test2';
-        $user2->password = Hash::make('test2');
-        $user2->email    = 'test2@homestead.app';
+        $user2->username = 'Sokrates';
+        $user2->password = Hash::make('secret');
+        $user2->email    = 'sokrates@homestead.app';
         $user2->active   = true;
-        $user2->handle   = 'test2';
+        $user2->handle   = 'sokrates';
         $user2->save();
 
         $member2             = new Member();
-        $member2->firstname  = 'Testnutzer';
-        $member2->lastname   = 'Zwei';
-        $member2->nickname   = 'Testus II.';
+        $member2->firstname  = 'Michael';
+        $member2->lastname   = 'Schmidt';
+        $member2->nickname   = 'Sokrates';
         $member2->profession = 'Holzfäller';
         $member2->birthday   = Carbon::createFromDate(1993, 3, 14);
         $member2->active     = true;
         $user2->member()->save($member2);
 
         $user3           = new User();
-        $user3->username = 'Test3';
-        $user3->password = Hash::make('test3');
-        $user3->email    = 'test3@homestead.app';
+        $user3->username = 'Quax';
+        $user3->password = Hash::make('secret');
+        $user3->email    = 'quax@homestead.app';
         $user3->active   = true;
-        $user3->handle   = 'test3';
+        $user3->handle   = 'quax';
         $user3->save();
 
         $member3             = new Member();
-        $member3->firstname  = 'Testnutzer';
-        $member3->lastname   = 'Drei';
-        $member3->nickname   = 'Testus III.';
+        $member3->firstname  = 'Sebastian';
+        $member3->lastname   = 'Bruch';
+        $member3->nickname   = 'Quax';
         $member3->profession = 'Astronaut';
         $member3->birthday   = Carbon::createFromDate(1997, 1, 31);
         $member3->active     = true;
